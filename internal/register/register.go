f7019e20917b893addd86cca1e54aa2dbc330289
budhi <budhiw@protonmail.com>
2026-01-28 23:28:53 +0700

    fix rank lookup mismatch

--- internal/register/register.go
+++ internal/register/register.go
@@ -80,22 +80,22 @@ func parseCheckbox(value string) bool {
 
 // allowedRanks defines the valid rank values for validation.
 var allowedRanks = map[string]bool{
-	"":         true, // empty is allowed
-	"10th Kyu": true,
-	"9th Kyu":  true,
-	"8th Kyu":  true,
-	"7th Kyu":  true,
-	"6th Kyu":  true,
-	"5th Kyu":  true,
-	"4th Kyu":  true,
-	"3rd Kyu":  true,
-	"2nd Kyu":  true,
-	"1st Kyu":  true,
-	"Shodan":   true,
-	"Nidan":    true,
-	"Sandan":   true,
-	"Yondan":   true,
-	"Godan":    true,
+	"":                 true, // empty is allowed
+	"10th Kyu":         true,
+	"9th Kyu":          true,
+	"8th Kyu":          true,
+	"7th Kyu":          true,
+	"6th Kyu":          true,
+	"5th Kyu":          true,
+	"4th Kyu":          true,
+	"3rd Kyu":          true,
+	"2nd Kyu":          true,
+	"1st Kyu":          true,
+	"Shodan (1st Dan)": true,
+	"Nidan (2nd Dan)":  true,
+	"Sandan (3rd Dan)": true,
+	"Yondan (4th Dan)": true,
+	"Godan (5th Dan)":  true,
 }
 
 // wantsJSON checks if the client expects or sends JSON.
